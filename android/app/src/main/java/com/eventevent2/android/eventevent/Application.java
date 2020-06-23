45a4c9b5314dd5f52d63891b353da077c68b80ab
reingalrt <adorufus@gmail.com>
2020-06-23 15:09:30 +0700

    fix android error

--- android/app/src/main/java/com/eventevent2/android/eventevent/Application.java
+++ android/app/src/main/java/com/eventevent2/android/eventevent/Application.java
@@ -1,15 +1,19 @@
 package com.eventevent2.android.eventevent;
 
 import io.flutter.app.FlutterApplication;
- import io.flutter.plugin.common.PluginRegistry;
- import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
- import io.flutter.plugins.GeneratedPluginRegistrant;
- import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
- import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import com.clevertap.android.sdk.CleverTapAPI;
 
 public class Application extends FlutterApplication implements PluginRegistrantCallback {
   @Override
   public void onCreate() {
+    CleverTapAPI.setDebugLevel(3);
+    CleverTapAPI.setUIEditorConnectionEnabled(true);
     ActivityLifecycleCallback.register(this);
     super.onCreate();
     FlutterFirebaseMessagingService.setPluginRegistrant(this);
