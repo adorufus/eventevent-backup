0b3607c7cb76deb260994dc0fd3ed80da62032ba
Reingalrt <adorufus@gmail.com>
2020-07-29 14:28:29 +0700

    commit

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import GoogleMaps
 import Firebase
-import CleverTapSDK
+// import CleverTapSDK
 // import CleverTapPlugin
 
 @UIApplicationMain
@@ -12,13 +12,13 @@ import CleverTapSDK
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // FirebaseApp.Configure()
-    CleverTap.autoIntegrate()
+    // CleverTap.autoIntegrate()
     GeneratedPluginRegistrant.register(with: self)
-     GMSServices.provideAPIKey("AIzaSyDO-ES5Iy3hOfiwz-IMQ-tXhOtH9d01RwI")
+    GMSServices.provideAPIKey("AIzaSyDO-ES5Iy3hOfiwz-IMQ-tXhOtH9d01RwI")
      // CleverTapPlugin.sharedInstance().applicationDidLaunch(withOptions: launchOptions)
-     if #available(iOS 10.0, *) {
-       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-     }
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
