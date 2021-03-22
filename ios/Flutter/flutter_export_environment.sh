0ae01140abf698896e1169680b5ae4720a6164ca
reingalrt <adorufus@gmail.com>
2021-03-22 14:36:10 +0700

    commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/userx520/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/userx520/Development/eventevent-backup"
+export "FLUTTER_ROOT=/Users/userUser/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/userUser/iCloud Drive (Archive)/eventevent-backup"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/home/userx520/snap/flutter/common/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/userUser/Development/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=3.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
